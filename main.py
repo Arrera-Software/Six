bbd3ac82f0cc62e51c154fc9590f5a1959b7869e
Baptiste <baptiste.pauchet13@gmail.com>
2022-10-09 16:45:33 +0200

    Passage du module internet dans le fichier principal

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import subprocess
-import webbrowser
 from gtts import gTTS
+import webbrowser
+import requests
 import os
 import datetime
 import random
 import speech_recognition as sr
-from ModuleInternet import TestInternet,duckduckgoSearch,GrandRecherche
-import requests
 from tkinter import*
 from tkinter.messagebox import *
 from translate import*
@@ -34,6 +33,97 @@ def HourInf(h1):
         return True
     else :
         return False
+#Fontion du module internet
+def TestInternet():
+    try:
+        _ = requests.get("https://duckduckgo.com",timeout=5)
+        return True
+    except requests.ConnectionError :
+        return False
+def braveSearch(query):
+    with requests.session() as c:
+        url = 'https://search.brave.com/search?q='
+        urllink = requests.get(url+query+"&source=web")
+        lienBrave = urllink.url
+        webbrowser.open(lienBrave)
+def AmazonSearch(query):
+    with requests.session() as c:
+        url = 'https://www.amazon.fr/s?k='
+        urllink = requests.get(url+query)
+        lienAmazon = urllink.url
+        webbrowser.open(lienAmazon)
+def googleSearch(query):
+    with requests.session() as c:
+        url = 'https://www.google.com/search?q'
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        liengoogle = urllink.url
+        webbrowser.open(liengoogle)
+def duckduckgoSearch(query):
+    with requests.session() as c:
+        url = 'https://duckduckgo.com/?q'
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        lienduck = urllink.url
+        webbrowser.open(lienduck)   
+def QwantSearch(query):
+    with requests.session() as c:
+        url = 'https://www.qwant.com/?l=fr&q'
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        lienQwant = urllink.url
+        webbrowser.open(lienQwant)
+
+def EcosiaSearch(query):
+    with requests.session() as c:
+        url = 'https://www.ecosia.org/search'
+        query = {'q': query}
+        urllink = requests.get(url,query)
+        lienEcosia = urllink.url
+        webbrowser.open(lienEcosia) 
+def bingSearch(query):
+    with requests.session() as c:
+        url = "https://www.bing.com/search"
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        lienbing = urllink.url
+        webbrowser.open(lienbing)
+def WikipediaSearch(query):
+    with requests.session() as c:
+        url = 'https://fr.wikipedia.org/wiki/'
+        lienWiki = url+query
+        webbrowser.open(lienWiki)
+def ReversoSeacrch(query):
+    with requests.session() as c:
+        url = 'https://www.reverso.net/traduction-texte#sl=fra&tl=eng&text='
+        urllink = requests.get(url+query)
+        liengoogle = urllink.url
+        webbrowser.open(liengoogle) 
+def WordreferenceSearch(query):
+    with requests.session() as c:
+        url = 'https://www.wordreference.com/fren/'
+        lienWord = url+query
+        webbrowser.open(lienWord)
+def YTmusicSearch(query):
+    with requests.session() as c:
+        url = 'https://music.youtube.com/search'
+        query = {'q': query}
+        urllink = requests.get(url, params=query)
+        lienYTmusic = urllink.url
+        webbrowser.open(lienYTmusic)
+def DocPython():
+    webbrowser.open("https://docs.python.org/3")
+def DocArduino():
+    webbrowser.open("https://docs.python.org/3")
+def GrandRecherche(query):
+    googleSearch(query)
+    duckduckgoSearch(query)
+    QwantSearch(query)
+    EcosiaSearch(query)
+    bingSearch(query) 
+    braveSearch(query)
+
+#______________________
 def Ecriture(file,text):#Fonction d'écriture sur un fichier texte
     doc = open(file,"w")
     doc.truncate()
@@ -937,12 +1027,6 @@ if internet == True :
         if "navigateur internet" in statement :
             speak("Ok j'ouvre votre navigateur internet")
             webbrowser.open(LienMoteur)
-        if "voix du nord" in statement :
-            webbrowser.open("https://www.lavoixdunord.fr/hauts-de-france")
-        if "libération" in statement:
-             webbrowser.open("https://www.liberation.fr/")
-        if "flipboard" in statement :
-            webbrowser.open("https://flipboard.com/")
         if "programmation" in statement :  
             break
         if "résumé" in statement:
