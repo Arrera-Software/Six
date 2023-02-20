daf258c941bdab150ca4d14b2d2e3ad5608097d7
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-02-20 18:07:47 +0100

    Fixe de la calculatrice

--- objet/Calcule/calcule.py
+++ objet/Calcule/calcule.py
@@ -16,11 +16,11 @@ class Calcule:
         self.imgFois = PhotoImage(file="image/btnMultiplier.png")
         self.screen.title(titre)
         self.screen.config(bg=mainColor)
-        self.screen.maxsize(700,700)
-        self.screen.minsize(700,700)
-        self.main = Frame(self.screen,bg=mainColor,width=700,height=700)
-        self.top = Frame(self.main,bg=mainColor,width=700,height=175)
-        self.bottom = Frame(self.main,bg=mainColor,width=700,height=175)
+        self.screen.maxsize(600,500)
+        self.screen.minsize(600,500)
+        self.main = Frame(self.screen,bg=mainColor,width=600,height=600)
+        self.top = Frame(self.main,bg=mainColor,width=600,height=175)
+        self.bottom = Frame(self.main,bg=mainColor,width=600,height=175)
         self.mainNbComplex = Frame(self.screen,bg=mainColor,width=700,height=700)
         self.topNbComplex = Frame(self.mainNbComplex,bg=mainColor,width=700,height=175)
         self.bottomNbComplex = Frame(self.mainNbComplex,bg=mainColor,width=700,height=175)
@@ -32,11 +32,11 @@ class Calcule:
         self.screen.config(menu=self.calculatriceMenu)
         #CalculeBase()
         self.screen.mainloop()
-        pass
     
     def widget(self,mainColor,mainTextColor):
         #label
-        self.affichage = Label(self.screen,text="Affichage résultat",font=("arial","25"),bg=mainColor,fg=mainTextColor)
+        self.labelBienvenu = Label(self.screen,text="Calculatrice",font=("arial","25"),bg=mainColor,fg=mainTextColor)
+        self.affichage = Label(self.screen,text="Affichage résultat",font=("arial","15"),bg=mainColor,fg=mainTextColor)
         self.labeli1 = Label(self.topNbComplex,text="j",font=("arial","25"),bg=mainColor,fg=mainTextColor)
         self.labeli2 = Label(self.bottomNbComplex,text="j",font=("arial","25"),bg=mainColor,fg=mainTextColor)
         #btn
@@ -44,10 +44,10 @@ class Calcule:
         self.btnMoin = Button(self.screen,image=self.imgMoin,command=self.moin)
         self.btnFois = Button(self.screen,image=self.imgFois,command=self.fois)
         self.btnDiviser = Button(self.screen,image=self.imgDivise,command=self.diviser)
-        self.btnExit = Button(self.mainNbComplex,text="Exit",font=("arial","15"),bg="white",fg="black")
+        self.btnExit = Button(self.screen,text="Exit",font=("arial","20"),bg="white",fg="black")
         #entry
-        self.entryNombre1 = Entry(self.top,width=40,font=("arial","15"),relief=SOLID)
-        self.entryNombre2 = Entry(self.bottom,width=40,font=("arial","15"),relief=SOLID)
+        self.entryNombre1 = Entry(self.top,width=30,font=("arial","15"),relief=SOLID)
+        self.entryNombre2 = Entry(self.bottom,width=30,font=("arial","15"),relief=SOLID)
         self.entryNombre1NbComplex1 = Entry(self.topNbComplex,width=15,font=("arial","15"),relief=SOLID)
         self.entryNombre1NbComplex2 = Entry(self.topNbComplex,width=15,font=("arial","15"),relief=SOLID)
         self.entryNombre2NbComplex1 = Entry(self.bottomNbComplex,width=15,font=("arial","15"),relief=SOLID)
@@ -84,28 +84,29 @@ class Calcule:
         labelAutreCal.place(x=0,y=130)
     
     def AffichageFrameMain(self):
-        self.top.place(x=0,y=0)
-        self.bottom.place(x=0,y=475)
+        self.labelBienvenu.pack()
+        self.top.pack()
+        self.bottom.pack()
         self.entryNombre1.place(relx=0.5, rely=0.5, anchor=CENTER)
         self.entryNombre2.place(relx=0.5, rely=0.5, anchor=CENTER)
         
     def AffichageBTNOperateur(self):
-        self.affichage.place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.btnPlus.place(x=50,y=320)
-        self.btnMoin.place(x=600,y=320)
-        self.btnDiviser.place(x=500,y=320)
-        self.btnFois.place(x=150,y=320)
+        self.affichage.place(x=10,y=400)
+        self.btnPlus.place(x=30,y=320)
+        self.btnMoin.place(x=90,y=320)
+        self.btnDiviser.place(x=150,y=320)
+        self.btnFois.place(x=210,y=320)
         
     def AffichageFrameMainNbComplex(self):
-        self.topNbComplex.place(x=0,y=0)
-        self.bottomNbComplex.place(x=0,y=475)
+        self.topNbComplex.pack()
+        self.bottomNbComplex.pack()
         self.entryNombre1NbComplex1.place(x=100,y=100)
         self.entryNombre1NbComplex2.place(x=400,y=100)
         self.entryNombre2NbComplex1.place(x=100,y=100)
         self.entryNombre2NbComplex2.place(x=400,y=100)
         self.labeli1.place(x=575,y=90)
         self.labeli2.place(x=575,y=90)
-        self.btnExit.place(x=600,y=625)
+        self.btnExit.place(x=410,y=320)
     
     def AffichageMain(self):
         self.AffichageFrameMain()
@@ -131,23 +132,23 @@ class Calcule:
         return nb1_1,nb1_2,nb2_1,nb2_2   
     
     def ShowResultat(self,nb1,nb2,resultat,calc):
-        self.affichage.config(text=str(nb1)+calc+str(nb2)+" = "+str(resultat))
+        self.affichage.configure(text=str(nb1)+calc+str(nb2)+" = "+str(resultat))
     
     def plus(self):
         nb1,nb2 = self.RecuperationEntry()
-        resultat = str(CalculeBase.adition(nb1,nb1))
+        resultat = str(CalculeBase().adition(nb1,nb2))
         self.ShowResultat(nb1,nb2,resultat,"+")
     def moin(self):
         nb1,nb2 = self.RecuperationEntry()
-        resultat = str(CalculeBase.soustration(nb1,nb2))
+        resultat = str(CalculeBase().soustration(nb1,nb2))
         self.ShowResultat(nb1,nb2,resultat,"-")
     def fois(self):
         nb1,nb2 = self.RecuperationEntry()
-        resultat = str(CalculeBase.multiplication(nb1,nb2))
+        resultat = str(CalculeBase().multiplication(nb1,nb2))
         self.ShowResultat(nb1,nb2,resultat,"*")
     def diviser(self):
         nb1,nb2 = self.RecuperationEntry()
-        resultat = str(CalculeBase.division(nb1,nb2))
+        resultat = str(CalculeBase().division(nb1,nb2))
         self.ShowResultat(nb1,nb2,resultat,"/")
     
     def plusNbComplex(self):
@@ -169,8 +170,9 @@ class Calcule:
     
     def exitComplex(self):
         self.mainNbComplex.place_forget()
+        self.btnExit.place_forget()
         self.AffichageMain()
-        self.affichage.config(text="Affichage résultat",font=("arial","25"))
+        self.affichage.config(text="Affichage résultat")
         self.btnPlus.config(command=self.plus)
         self.btnMoin.config(command=self.moin)
         self.btnFois.config(command=self.fois)
@@ -179,7 +181,6 @@ class Calcule:
     def NbComplexe(self):
         self.AffichageFrameMainNbComplex()
         self.main.place_forget()
-        self.affichage.config(text="Cacule\nNombre Complex",font=("arial","15"))
         self.btnExit.config(command=self.exitComplex)
         self.btnPlus.config(command=self.plusNbComplex)
         self.btnMoin.config(command=self.moinNbComplex)
