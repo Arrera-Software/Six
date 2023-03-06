3abe53502800135fbbb1bf99ada18f20e2f2f5ff
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-03-06 18:54:08 +0100

    Ajout la fonction ouvretureTK et fermerureTK

--- neuron/neuronSoftware.py
+++ neuron/neuronSoftware.py
@@ -2,11 +2,14 @@ from software.YoutubeDownload import *
 from src.srcSix import*
 from objet.Calcule.calcule import *
 from function.openSofware import*
+from function.fenetrePygame import*
 
 def Software(var,genre,user,name,root,police):
+    fenetre = pygameFond(root,police,genre)
     if "enregistre de la musique" in var or "enregistrement de la musique" in var or "enregistre moi des vidéos" in var or "enregistre-moi une vidéo" in var:
-        SIXsrc(root,police).speak("Ok "+genre+" je vous ouvre le téléchargeur de video Youtube.")
+        fenetre.OuvertureTK("Ok "+genre+" je vous ouvre le téléchargeur de video Youtube.")
         ArreraVideoDownload("#3c0b10","white","#3c0b10","white")
+        fenetre.FermetureTK()
         return 1
     else :
         if "ouvre l'explorateur de fichier" in var or "ouvre les fichiers" in var or "montre-moi mes fichiers" in var :
@@ -15,8 +18,9 @@ def Software(var,genre,user,name,root,police):
             return 1
         else :
             if "calcul" in var or "calculer" in var :
-                SIXsrc(root,police).speak("Je vous ouvre ma calculatrice")
+                fenetre.OuvertureTK("Je vous ouvre ma calculatrice")
                 Calcule("#3c0f14","white","Six : Calcule")
+                fenetre.FermetureTK()
                 return 1
             else :
                 if "ouvre mes notes" in var or "ouvre les notes" in var or "ouvre-moi mes notes" in var :
--- neuron/neuronTime.py
+++ neuron/neuronTime.py
@@ -3,8 +3,10 @@ from src.srcSix import*
 from objet.Horloge.AppHorloge import*
 from objet.date.objetdate import*
 from function.calendar import *
+from function.fenetrePygame import*
 
 def neuronTime(var,genre,user,name,root,police):
+    fenetre = pygameFond(root,police,genre)
     if "heure" in var :
         hour = str(datetime.datetime.now().hour)
         minute = str(datetime.datetime.now().minute)
@@ -19,23 +21,28 @@ def neuronTime(var,genre,user,name,root,police):
             return 1
         else : 
             if "horloge" in var :
-                SIXsrc(root,police).speak("Ok je vous ouvre l'application horloge "+user,+" "+name)
+                fenetre.OuvertureTK("Ok je vous ouvre l'application horloge "+genre+" "+user)
                 AppHorloge("#3c0f14","white","Six : Horloge","acceuil")
+                fenetre.FermetureTK()
                 return 1
             else :
-                if "chronométre" in var or "chono" in var or "chonometre" in var :
-                    SIXsrc(root,police).speak("Ok je t'ouvre le chronometre "+genre+" "+user)
+                if "chronomètre" in var or "chrono" in var :
+                    fenetre.OuvertureTK("Ok je vous ouvre le chronomètre "+genre+" "+user)
                     AppHorloge("#3c0f14","white","Six : Horloge","chronometre")
+                    fenetre.FermetureTK()
                     return 1
                 else :
                     if "minuteur" in var :
-                        SIXsrc(root,police).speak("Ok je t'ouvre le minuteur "+genre+" "+user)
+                        fenetre.OuvertureTK("Ok je vous ouvre le minuteur "+genre+" "+user)
                         AppHorloge("#3c0f14","white","Six : Horloge","minuteur")
+                        fenetre.FermetureTK()
                         return 1
                     else :
                         if "agenda" in var :
                             if "ouvre" in var :
+                                fenetre.OuvertureTK("Je vous ouvre mon calendrier")
                                 SixCalendar()
+                                fenetre.FermetureTK()
                             else :
                                 texte = SortieEvenementTexte()
                                 SIXsrc(root,police).speak(texte)
