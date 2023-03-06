c58154a7400859acde58d3eba5550ef30e3775c0
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-03-06 18:10:23 +0100

    Ajout d'une case de debugage de la fenetre pygame

--- script.py
+++ script.py
@@ -4,9 +4,11 @@ from objet.Calcule.calcule import *
 from objet.Horloge.AppHorloge import*
 from src.varInterface import*
 from function.api import*
+from src.varInterface import*
 from function.calendar import*
+import pygame
 
-rep = int(input("Outil debugage:\n1.Parametre\n2.Youtube download\n3.Calcule\n4.Application Horloge\n5.Actualités description\n6.Calendrier\n0.Quitter\n$ "))
+rep = int(input("Outil debugage:\n1.Parametre\n2.Youtube download\n3.Calcule\n4.Application Horloge\n5.Actualités description\n6.Calendrier\n7.Fenetre Pygame\n0.Quitter\n$ "))
 
 match rep :
     case 1 :
@@ -22,6 +24,60 @@ match rep :
     case 6 :
       SixCalendar()
     case 7 :
-      Agenda("objet/calendrier/agenda.json").SupprEnvent(1)
+      os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (20,35)
+      pygame.init()
+      pygame.display.set_icon(pygame.image.load("image/logo.png"))
+      pygame.display.set_caption("Assistant SIX")
+      root  = pygame.display.set_mode((rootWidht,rootHeight))
+      police = pygame.font.SysFont("arial", 25)
+      var = True 
+      root.blit(fond, (0, 0))
+      pygame.display.update()
+      while var:
+          for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                var = False
+              else :
+                if event.type == pygame.KEYDOWN:
+                  if event.key == pygame.K_a:
+                    root.blit(fondParole1, (0, 0))
+                    pygame.display.update()
+                    time.sleep(5)
+                  else :
+                    if event.key == pygame.K_z :
+                      root.blit(fondFenetre, (0, 0))
+                      pygame.display.update()
+                      time.sleep(5)
+                    else :
+                      if event.key == pygame.K_e:
+                        root.blit(fondMute, (0, 0))
+                        pygame.display.update()
+                        time.sleep(5)
+                      else :
+                        root.blit(fond, (0, 0))
+                        pygame.display.update()
+                else :
+                  root.blit(fond, (0, 0))
+                  pygame.display.update()
+      
+                    
+                  
+    
+      pygame.quit()
+    
     case other:
         print("sa correspont pas")
+
+
+"""
+
+            if event.key == pygame.K_a:
+                # Action lorsque la touche 'a' est appuyée
+                print("La touche 'a' a été appuyée")
+            elif event.key == pygame.K_z:
+                # Action lorsque la touche 'z' est appuyée
+                print("La touche 'z' a été appuyée")
+            elif event.key == pygame.K_e:
+                # Action lorsque la touche 'e' est appuyée
+                print("La touche 'e' a été appuyée")
+"""
