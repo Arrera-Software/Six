bf9aeaf69f33282786b95b1224b18fb4e72ecf5d
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-03-06 18:09:03 +0100

    Ajout de l'image qui s'affichera quand une fenetre tkinter s'ouvrira

--- src/varInterface.py
+++ src/varInterface.py
@@ -6,6 +6,7 @@ lienMute = ["image/interface/interfaceDefaultMute.png","image/interface/interfac
 lienParole1 = ["image/interface/interfaceDefaultVoice1.png","image/interface/interfaceLIghtVoice1.png","image/interface/interfaceDarkVoice1.png"]
 lienParole2 = ["image/interface/interfaceDefaultVoice2.png","image/interface/interfaceLIghtVoice2.png","image/interface/interfaceDarkVoice2.png"]
 lienParole3 = ["image/interface/interfaceDefaultVoice3.png","image/interface/interfaceLIghtVoice3.png","image/interface/interfaceDarkVoice3.png"]
+lienFenetre = ["image/interface/interfaceDefaultFenetre.png","image/interface/interfaceLIghtFenetre.png","image/interface/interfaceDarkFenetre.png"]
 
 principalUser =  lectureJSON("setting/config.json","user1")
 secondairUser =  lectureJSON("setting/config.json","user2")
@@ -74,10 +75,21 @@ def GUItextColor():
         else :
             return (255,255,255) 
 
+def GUIOpenFentreTkinter():
+    var = lectureJSON("setting/config.json","theme")
+    if var == "default" :
+        return pygame.image.load(lienFenetre[0])
+    else :
+        if var == "white":
+            return pygame.image.load(lienFenetre[1])
+        else :
+            return pygame.image.load(lienFenetre[2])
+
 rootWidht = 600
 rootHeight = 200
 fondMute = GUIMute()
 fond = GUIMain()
+fondFenetre = GUIOpenFentreTkinter()
 fondParole1 = GUIparole1()
 fondParole2 = GUIparole2()
 fondParole3 = GUIparole3()
