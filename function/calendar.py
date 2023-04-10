3cbadde8b04a2a1f2507d0fd4f4fc3853b5b65fc
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-04-10 15:01:11 +0200

    Passage de la fenetre calendar en prog objet est changement de la couleur de l'interface

--- function/calendar.py
+++ function/calendar.py
@@ -5,33 +5,86 @@ from tkinter.messagebox import showinfo
 from tkinter import*
 
     
-def SixCalendar():
-    screen = Tk()
-    selected_date = StringVar()
-    varSupprEvent = StringVar(screen)
-    screen.title("Six : agenda du jour")
-    screen.iconphoto(False,PhotoImage(file="image/logo.png"))
-    screen.maxsize(500,300)
-    screen.minsize(500,300)
-    screen.config(bg="white")
-    listeEvent = []
-    #fonction
-    def SupprEvent():
+class SixCalendar():
+    def __init__(self):
+        self.screen = Tk()
+        self.selected_date = StringVar()
+        self.varSupprEvent = StringVar(self.screen)
+        self.screen.title("Six : agenda du jour")
+        self.screen.iconphoto(False,PhotoImage(file="image/logo.png"))
+        self.screen.maxsize(500,300)
+        self.screen.minsize(500,300)
+        self.screen.config(bg="#3c0b10")
+        self.listeEvent = []
+        #carde
+        self.cadreToday = Frame(self.screen,width=500,height=233,bg="#3c0b10")
+        self.cadreEvent = Frame(self.screen,width=500,height=233,bg="#3c0b10")
+        self.cadreSuppr = Frame(self.screen,width=500,height=233,bg="#3c0b10")
+        #menu
+        self.menuBar  =  Menu(self.screen,bg="#3c0b10",fg="white")
+        self.menuBar.add_command(label="Créer un événement",command=self.createEvent)
+        self.menuBar.add_command(label="Supprimmer un événement",command=self.SupprEvent)
+        #label
+        self.labelToday = Label(self.cadreToday,bg="#3c0b10",fg="white",font=("arial","20"),text="Aujourd'hui : "+dateToday().jour()+" "+dateToday().moisSTR()+" "+dateToday().annes())
+        self.labelFeteToday = Label(self.cadreToday,font=("arial","15"),bg="#3c0b10",fg="white")
+        self.labelIndicationName = Label(self.cadreEvent,text="Name :",bg="#3c0b10",fg="white",font=("arial","13"))
+        self.labelAffichageDate = Label(self.cadreEvent,bg="#3c0b10",fg="#3c0b10",font=("arial","15"))
+        #Entry
+        self.entryName = Entry(self.cadreEvent,width=20,font=("arial",15),relief=SOLID)
+        #Bouton 
+        self.BoutonValiderEvent  = Button(self.cadreEvent,command=self.ValiderAddEvent,text="Valider",bg="#3c0b10",fg="white",font=("arial","15")) 
+        self.boutonVadiderSuppr = Button(self.cadreSuppr,text="Valider",bg="#3c0b10",fg="white",font=("arial","15"))
+        #option menu
+        self.compteurEvent = compteurJSON("objet/calendrier/agenda.json")
+        if self.compteurEvent == 0 :
+            self.labelFeteToday.configure(text="Fête du jour : "+CalendrierToday().SaintDujour())
+        else :
+            valDateToday = dateToday().annes()+"-"+dateToday().moisINT()+"-"+dateToday().jour()
+            listEvent = Agenda("objet/calendrier/agenda.json").ListEventDate()
+            self.labelFeteToday.configure(text="Fête du jour : "+CalendrierToday().SaintDujour()+"\nProgramme du jour :")
+            i = 1 
+            while i <= self.compteurEvent :
+                dateEvent = listEvent[str(i)]
+                if valDateToday == dateEvent :
+                    listEventName = Agenda("objet/calendrier/agenda.json").ListEventName()[str(i)]
+                    self.labelFeteToday.configure(text=self.labelFeteToday.cget("text")+"\n"+listEventName)
+                    i = i+1
+                else :
+                    i = i+1
+    
+        self.cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
+        
+        self.labelToday.place(x=0,y=0)
+        self.labelIndicationName.place(x=0,y=50)
+        self.labelFeteToday.place(relx=0.5,rely=0.5,anchor=CENTER)
+        
+        self.BoutonValiderEvent.place(relx=0.5, rely=1.0, anchor='s')
+        self.boutonVadiderSuppr.place(relx=0.5, rely=1.0, anchor='s')
+        
+        self.entryName.place(x=70,y=50)
+        
+    
+        
+        self.screen.config(menu=self.menuBar)
+        
+        self.screen.mainloop()
+    
+    def SupprEvent(self):
         if len(lectureSimpleJSON("objet/calendrier/agenda.json")) == 0 :
            showinfo(title="Six : agenda du jour",message="Il n'a pas d'événement enregister")  
         else :
-            cadreToday.place_forget()
-            cadreSuppr.place(relx=0.5,rely=0.5,anchor=CENTER)
+            self.cadreToday.place_forget()
+            self.cadreSuppr.place(relx=0.5,rely=0.5,anchor=CENTER)
             dictEvent = Agenda("objet/calendrier/agenda.json").ListEventName()
             compteur = compteurJSON("objet/calendrier/agenda.json")
             i = 1 
             while i<= compteur :
-                listeEvent.append(dictEvent[str(i)])
+                self.listeEvent.append(dictEvent[str(i)])
                 i = i + 1 
-            menuOptionSuppr = OptionMenu(cadreSuppr,varSupprEvent,*listeEvent)
+            menuOptionSuppr = OptionMenu(self.cadreSuppr,self.varSupprEvent,*self.listeEvent)
             menuOptionSuppr.place(relx=0.5,rely=0.5,anchor=CENTER)
             def valider():
-                var = varSupprEvent.get()
+                var = self.varSupprEvent.get()
                 i = 1 
                 while i<= compteur :
                     valeur = dictEvent.get(str(i))
@@ -40,22 +93,24 @@ def SixCalendar():
                         i = compteur + 1
                     else :
                         i = i + 1 
-                cadreSuppr.place_forget()
-                listeEvent.clear()
+                self.cadreSuppr.place_forget()
+                self.listeEvent.clear()
                 menuOptionSuppr.destroy()
-                cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
-            boutonnVadiderSuppr.config(command=valider)
-    def createEvent():
-        cadreToday.place_forget()
-        cadreEvent.place(relx=0.5,rely=0.5,anchor=CENTER)
-    def ValiderAddEvent():
-        name = entryName.get()
+                self.cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
+            self.boutonVadiderSuppr.config(command=valider)
+    
+    def createEvent(self):
+        self.cadreToday.place_forget()
+        self.cadreEvent.place(relx=0.5,rely=0.5,anchor=CENTER)
+        
+    def ValiderAddEvent(self):
+        name = self.entryName.get()
         def SortieDate():
             date =  str(calendrier.selection_get().strftime('%Y-{0}-%d'.format(calendrier.selection_get().strftime('%m').lstrip('0'))))
-            labelAffichageDate.configure(text=date)
+            self.labelAffichageDate.configure(text=date)
             Agenda("objet/calendrier/agenda.json").addEvent(name,date)
-            cadreEvent.place_forget()
-            cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
+            self.cadreEvent.place_forget()
+            self.cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
             top.destroy()
         if name == "":
             showinfo(title="Six : agenda du jour",message="Veuiller donner un nom a l'evenement")      
@@ -64,82 +119,29 @@ def SixCalendar():
             calendrier = Calendar(top, selectmode="day")
             calendrier.pack()
             Button(top, text="Valider", command=SortieDate).pack()
-        
-    #carde
-    cadreToday = Frame(screen,width=500,height=233,bg="white")
-    cadreEvent = Frame(screen,width=500,height=233,bg="white")
-    cadreSuppr = Frame(screen,width=500,height=233,bg="white")
-    #menu
-    menuBar  =  Menu(screen,bg="white",fg="black")
-    menuBar.add_command(label="Créer un événement",command=createEvent)
-    menuBar.add_command(label="Supprimmer un événement",command=SupprEvent)
-    #label
-    labelToday = Label(cadreToday,bg="white",fg="#3c0b10",font=("arial","20"),text="Aujourd'hui : "+dateToday().jour()+" "+dateToday().moisSTR()+" "+dateToday().annes())
-    labelFeteToday = Label(cadreToday,font=("arial","15"),bg="white",fg="#3c0b10")
-    labelIndicationName = Label(cadreEvent,text="Name :",bg="white",fg="#3c0b10",font=("arial","13"))
-    labelAffichageDate = Label(cadreEvent,bg="white",fg="#3c0b10",font=("arial","15"))
-    #Entry
-    entryName = Entry(cadreEvent,width=20,font=("arial",15),relief=SOLID)
-    #Bouton 
-    BoutonValiderEvent  = Button(cadreEvent,command=ValiderAddEvent,text="Valider",bg="white",fg="#3c0b10",font=("arial","15")) 
-    boutonnVadiderSuppr = Button(cadreSuppr,text="Valider",bg="white",fg="#3c0b10",font=("arial","15"))
-    #option menu
-    compteurEvent = compteurJSON("objet/calendrier/agenda.json")
-    if compteurEvent == 0 :
-        labelFeteToday.configure(text="Fête du jour : "+CalendrierToday().SaintDujour())
-    else :
-        valDateToday = dateToday().annes()+"-"+dateToday().moisINT()+"-"+dateToday().jour()
-        listEvent = Agenda("objet/calendrier/agenda.json").ListEventDate()
-        labelFeteToday.configure(text="Fête du jour : "+CalendrierToday().SaintDujour()+"\nProgramme du jour :")
-        i = 1 
-        while i <= compteurEvent :
-            dateEvent = listEvent[str(i)]
-            if valDateToday == dateEvent :
-                listEventName = Agenda("objet/calendrier/agenda.json").ListEventName()[str(i)]
-                labelFeteToday.configure(text=labelFeteToday.cget("text")+"\n"+listEventName)
-                i = i+1
-            else :
-                i = i+1
- 
-    cadreToday.place(relx=0.5,rely=0.5,anchor=CENTER)
-    
-    labelToday.place(x=0,y=0)
-    labelIndicationName.place(x=0,y=50)
-    labelFeteToday.place(relx=0.5,rely=0.5,anchor=CENTER)
-    
-    BoutonValiderEvent.place(relx=0.5, rely=1.0, anchor='s')
-    boutonnVadiderSuppr.place(relx=0.5, rely=1.0, anchor='s')
-    
-    entryName.place(x=70,y=50)
-    
-   
-    
-    screen.config(menu=menuBar)
-    
-    screen.mainloop()
-    
-    
+            
+
 def SortieEvenementTexte():
-    varAngenda = Agenda("objet/calendrier/agenda.json")
-    compteurEvent = compteurJSON("objet/calendrier/agenda.json")
-    if compteurEvent == 0 :
-        texte =  "Vous avez rien de prevu aujourd'hui"
-    else :
-        valDateToday = dateToday().annes()+"-"+dateToday().moisINT()+"-"+dateToday().jour()
-        listEventDate = varAngenda.ListEventDate()
-        listeEnvetName = varAngenda.ListEventName()
-       
-        if compteurEvent > 1 : 
-            texte = "Vous avez quelque chose de prévue ajourd'hui qui sont "
+        varAngenda = Agenda("objet/calendrier/agenda.json")
+        compteurEvent = compteurJSON("objet/calendrier/agenda.json")
+        if compteurEvent == 0 :
+            texte =  "Vous avez rien de prevu aujourd'hui"
         else :
-            texte = "Vous avez quelque chose de prévue ajourd'hui qui est "
-        i = 1 
-        while i <= compteurEvent :
-            dateEvent = listEventDate[str(i)]
-            nameEvent = listeEnvetName[str(i)]
-            if valDateToday == dateEvent :
-                texte = texte + " "+nameEvent
-                i = i + 1
+            valDateToday = dateToday().annes()+"-"+dateToday().moisINT()+"-"+dateToday().jour()
+            listEventDate = varAngenda.ListEventDate()
+            listeEnvetName = varAngenda.ListEventName()
+        
+            if compteurEvent > 1 : 
+                texte = "Vous avez quelque chose de prévue ajourd'hui qui sont "
             else :
-                i = i+1
-    return texte
+                texte = "Vous avez quelque chose de prévue ajourd'hui qui est "
+            i = 1 
+            while i <= compteurEvent :
+                dateEvent = listEventDate[str(i)]
+                nameEvent = listeEnvetName[str(i)]
+                if valDateToday == dateEvent :
+                    texte = texte + " "+nameEvent
+                    i = i + 1
+                else :
+                    i = i+1
+        return texte
