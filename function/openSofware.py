be6ec58f51eb26ff6e69a09d20f6b599f94c5b6a
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-09-02 13:37:38 +0200

    SUppression fichier inutile

--- function/openSofware.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def openSoftware(name):
-    name = str(name)
-    subprocess.Popen(["cmd", "/c", "start", name])
-    
-def ouvertureAide():
-    openSoftware("raccourci/index.html.lnk")
--- objet/Horloge/HorlogeAcceuil.py
+++
@@ -1,7 +0,0 @@
-from tkinter import*
-
-class Acceuil : 
-    def __init__(self,master,mainColor,textColor,img):
-        self.master = master
-        self.label = Label(master,image=img,bg=mainColor)
-        self.label.place(relx=0.5, rely=0.5, anchor=CENTER)
