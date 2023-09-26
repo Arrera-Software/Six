13bae1afeaa9d34c0809ce935ac60097d4b0c3cd
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-09-26 18:30:22 +0200

    mise en place de la nouvelle interface

--- src/SIXGestion.py
+++ src/SIXGestion.py
@@ -12,12 +12,15 @@ class SIXGestion :
         except requests.ConnectionError :
             self.etatConnexion = False
         #Emplacement image interface
-        self.lienMain = ["asset/IMGinterface/interfaceDefault.png","asset/IMGinterface/interfaceLIght.png","asset/IMGinterface/interfaceDark.png"]
-        self.lienMute = ["asset/IMGinterface/interfaceDefaultMute.png","asset/IMGinterface/interfaceLIghtMute.png","asset/IMGinterface/interfaceDarkMute.png"]
-        self.lienParole1 = ["asset/IMGinterface/interfaceDefaultVoice1.png","asset/IMGinterface/interfaceLIghtVoice1.png","asset/IMGinterface/interfaceDarkVoice1.png"]
-        self.lienParole2 = ["asset/IMGinterface/interfaceDefaultVoice2.png","asset/IMGinterface/interfaceLIghtVoice2.png","asset/IMGinterface/interfaceDarkVoice2.png"]
-        self.lienParole3 = ["asset/IMGinterface/interfaceDefaultVoice3.png","asset/IMGinterface/interfaceLIghtVoice3.png","asset/IMGinterface/interfaceDarkVoice3.png"]
-        self.lienFenetre = ["asset/IMGinterface/interfaceDefaultFenetre.png","asset/IMGinterface/interfaceLIghtFenetre.png","asset/IMGinterface/interfaceDarkFenetre.png"]
+        self.lienAcceuil = str
+        self.lienMute = str
+        self.lienMain = str
+        self.lienAttent = [str]
+        self.lienParole = [str]
+        self.lienMicro = [str]
+        self.colorText = (int,int,int)
+        #Creation var de verification
+        self.themeSet = bool
         #Ouverture fichier config Six
         self.fileSixConfig = jsonWork("sixConfig.json")
         #varriable theme
@@ -27,102 +30,75 @@ class SIXGestion :
         return self.etatConnexion
        
     def setTheme(self):
-        self.varTheme = self.fileSixConfig.lectureJSON("theme") #Valeur possible "default","white","dark"
+        varTheme = self.fileSixConfig.lectureJSON("theme") #Valeur possible "default","white","dark"
+        #Non de fichier Image
+        acceuil = "IMGAcceuil.png"
+        main = "IMGmain.png"
+        attent1 = "IMGAttent1.png"
+        attent2 = "IMGAttent2.png"
+        attent3 = "IMGAttent3.png"
+        microSmallSmall = "IMGMicro-Small-Small.png" 
+        microSmallBig = "IMGMicroSmall-Big.png"
+        mute = "IMGMute.png"
+        speakBigSmall = "IMGSpeak-BIG-Small.png"
+        speakSmallSmall = "IMGSpeak-Small-Small.png"
+        if varTheme == "default" :
+            chemin = "asset/IMGinterface/default/"
+            self.colorText = (0,0,0)
+        else :
+            if varTheme == "black" : 
+               chemin = "asset/IMGinterface/black/"
+               self.colorText = (0,0,0)
+            else :
+                if varTheme == "white" :
+                    chemin = "asset/IMGinterface/white/"
+                    self.colorText = (255,255,255)
+                else:
+                    chemin = "asset/IMGinterface/default/"
+                    self.colorText = (0,0,0)
+        self.lienAcceuil = chemin+acceuil
+        self.lienMute = chemin+mute
+        self.lienMain = chemin+main
+        self.lienAttent = [chemin+attent1,
+                            chemin+attent2,
+                            chemin+attent3]
+        self.lienParole = [chemin+speakBigSmall,
+                            chemin+speakSmallSmall]
+        self.lienMicro = [chemin+microSmallBig,
+                            chemin+microSmallSmall]
+        
+        self.themeSet = True
+    
+    def getGUIAcceuil(self):
+        if self.themeSet == True :
+            return pygame.image.load(self.lienAcceuil)
     
     def getGUIMain(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienMain[0])
-        else :
-            if self.varTheme == "white" :
-                return pygame.image.load(self.lienMain[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienMain[2])
-                else :
-                    return pygame.image.load(self.lienMain[0])
-
-
+        if self.themeSet == True :
+            return pygame.image.load(self.lienMain)
+       
     def getGUIMute(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienMute[0])
-        else :
-            if self.varTheme == "white" :
-                return pygame.image.load(self.lienMute[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienMute[2])
-                else :
-                    return pygame.image.load(self.lienMute[0])
+        if self.themeSet == True :
+            return pygame.image.load(self.lienMute)
 
-    def getGUIparole1(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienParole1[0])
-        else :
-            if self.varTheme == "white" :
-                return pygame.image.load(self.lienParole1[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienParole1[2])
-                else :
-                    return pygame.image.load(self.lienParole1[0])   
+    def getGUIparoleBigSmall(self):
+        if self.themeSet == True :
+            return pygame.image.load(self.lienParole[0])
 
-    def getGUIparole2(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienParole2[0])
-        else :
-            if self.varTheme == "white" :
-                return pygame.image.load(self.lienParole2[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienParole2[2])  
-                else :
-                    return pygame.image.load(self.lienParole2[0])
+        
+    def getGUIparoleSmallSmall(self):
+        if self.themeSet == True :
+            return pygame.image.load(self.lienParole[1])
+    
 
-    def getGUIparole3(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienParole3[0])
-        else :
-            if self.varTheme == "white" :
-                return pygame.image.load(self.lienParole3[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienParole3[2])
-                else :
-                    return pygame.image.load(self.lienParole3[0]) 
-                
+    def getGUImicroSmallBig(self):
+        if self.themeSet == True :
+            return pygame.image.load(self.lienMicro[0])
+        
+    def getGUImicroSmallSmall(self):
+        if self.themeSet == True :
+            return pygame.image.load(self.lienMicro[1])          
 
     def getGUItextColor(self):
-        if self.varTheme == "default" :
-            return (255,255,255)
-        else :
-            if self.varTheme == "white" :
-                return (0,0,0)
-            else :
-                if self.varTheme == "dark" :
-                    return (255,255,255)
-                else :
-                    return (255,255,255)
-                    
-
-    def getGUIOpenFentreTkinter(self):
-        if self.varTheme == "default" :
-            return pygame.image.load(self.lienFenetre[0])
-        else :
-            if self.varTheme == "white":
-                return pygame.image.load(self.lienFenetre[1])
-            else :
-                if self.varTheme == "dark" :
-                    return pygame.image.load(self.lienFenetre[2])
-                else :
-                    return pygame.image.load(self.lienFenetre[0])
-"""
-rootWidht = 600
-rootHeight = 200
-fondMute = GUIMute()
-fond = GUIMain()
-fondFenetre = GUIOpenFentreTkinter()
-fondParole1 = GUIparole1()
-fondParole2 = GUIparole2()
-fondParole3 = GUIparole3()
-ColorText = GUItextColor()
-"""
+        if self.themeSet == True:
+            return self.colorText
--- src/sixInterface.py
+++ src/sixInterface.py
@@ -3,17 +3,19 @@ import pygame
 import time as t
 from src.SIXGestion import*
 
-
 class SIXInterface:
     def __init__(self,objetGestion:SIXGestion):
         self.rootWidht = 600
-        self.rootHeight = 200
-        self.fond = objetGestion.getGUIMain()
-        self.mainFond = objetGestion.getGUIMain()
-        self.listFondParole = [objetGestion.getGUIparole1(),
-                               objetGestion.getGUIparole2(),
-                               objetGestion.getGUIparole3()]
+        self.rootHeight = 500
+        self.mainGUI = objetGestion.getGUIMain()
+        self.AcceuilGUI = objetGestion.getGUIAcceuil()
+        self.paroleGUI = [objetGestion.getGUIparoleBigSmall(),
+                               objetGestion.getGUIparoleSmallSmall()]
+        self.microGUI = [objetGestion.getGUImicroSmallBig(),
+                         objetGestion.getGUImicroSmallSmall()]
+            
         self.colorText = objetGestion.getGUItextColor()
+        self.oldRequette = str
         
         
     def initialisationFenetre(self):
@@ -22,48 +24,115 @@ class SIXInterface:
         pygame.display.set_icon(pygame.image.load("asset/logo.png"))
         pygame.display.set_caption("Assistant SIX")
         self.root  = pygame.display.set_mode((self.rootWidht,self.rootHeight),pygame.NOFRAME)
+        self.policeTitre = pygame.font.SysFont("arial", 30)
         self.police = pygame.font.SysFont("arial", 25)
-        self.root.blit(self.fond.convert(),(0,0))
+        self.root.blit(self.mainGUI.convert(),(0,0))
         self.formatSpeak = pygame.font.SysFont("arial", 20)
         pygame.display.update()
-        
-    def interfaceSpeak(self,text1:str,text2:str,text3:str):
-        texte1 = text1
-        texte2 = text2
-        texte3 = text3
-        self.root.blit(self.listFondParole[1].convert(),(0,0))
-        pygame.display.update()
-        t.sleep(1)
-        labelAssistant1 = self.formatSpeak.render(texte1,1,(self.colorText))
-        labelAssistant2 = self.formatSpeak.render(texte2,1,(self.colorText))
-        labelAssistant3 = self.formatSpeak.render(texte3,1,(self.colorText))
-        textRect1 = labelAssistant1.get_rect(center=(600/2 ,55))
-        textRect2 = labelAssistant2.get_rect(center=(600/2 ,75))
-        textRect3 = labelAssistant3.get_rect(center=(600/2 ,95))
-        self.root.blit(self.listFondParole[2].convert(),(0,0))
-        self.root.blit(labelAssistant1,textRect1)
-        pygame.display.update()
-        t.sleep(1)
-        self.root.blit(self.listFondParole[0].convert(),(0,0))
-        self.root.blit(labelAssistant2,textRect2)
-        pygame.display.update()
-        t.sleep(1)
-        self.root.blit(labelAssistant3,textRect3)
-        self.root.blit(self.listFondParole[1].convert(),(0,0))
-        pygame.display.update()
-        t.sleep(1)
-        self.root.blit(self.mainFond.convert(),(0,0))
-        pygame.display.update()
-        
-    def interfaceMicro(self,requette):
-        print(requette)
-        self.root.blit(self.mainFond.convert(),(0,0))
-        pygame.display.update()
-        t.sleep(1)
-        labelUser = self.police.render(requette,1,(self.colorText))
-        text_rect = labelUser.get_rect(center=(self.rootWidht/2 ,self.rootHeight/2))
-        self.root.blit(labelUser,text_rect)
-        pygame.display.update()
-        t.sleep(1)
-        
     
+    def interfaceBoot(self,text:str):
+        texte = text
+        self.root.blit(self.AcceuilGUI.convert(),(0,0))
+        if self._compteur(texte) >= 4 :
+            text1,text2 = self._division(texte,4)
+            if self._compteur(text2) >= 4 :
+                text2,text3 = self._division(text2,4)
+                labelText1 = self.policeTitre.render(text1,1,(self.colorText))
+                labelText2 = self.policeTitre.render(text2,1,(self.colorText))
+                labelText3 = self.policeTitre.render(text3,1,(self.colorText))
+                textRect1 = labelText1.get_rect(center=(600/2 ,240))
+                textRect2 = labelText2.get_rect(center=(600/2 ,270))
+                textRect3 = labelText3.get_rect(center=(600/2 ,300))
+                self.root.blit(labelText1,textRect1)
+                self.root.blit(labelText2,textRect2)
+                self.root.blit(labelText3,textRect3)
+            else :
+                labelText1 = self.policeTitre.render(text1,1,(self.colorText))
+                labelText2 = self.policeTitre.render(text2,1,(self.colorText))
+                textRect1 = labelText1.get_rect(center=(600/2 ,240))
+                textRect2 = labelText2.get_rect(center=(600/2 ,270))
+                self.root.blit(labelText1,textRect1)
+                self.root.blit(labelText2,textRect2)
+        else :
+            labelText = self.policeTitre.render(texte,1,(self.colorText))
+            textRect = labelText.get_rect(center=(600/2 ,240))
+            self.root.blit(labelText,textRect)
+        self.speakBig = True
+        self.oldSpeak = texte
+        pygame.display.update()
+
+        
+    def interfaceSpeak(self,texte:str):
+        nbMots = self._compteur(texte)
+        print(nbMots)
+        if nbMots > 6 :
+            text1,text2 = self._division(texte,6)
+            nbMots = self._compteur(text2)
+            if nbMots > 6 :
+                guiParole = self.paroleGUI[0] #Big
+                self.root.blit(guiParole.convert(),(0,0))
+                text2,text3 = self._division(text2,6)
+                labelText1 =  self.police.render(text1,1,(self.colorText))
+                labelText2 =  self.police.render(text2,1,(self.colorText))
+                labelText3 =  self.police.render(text3,1,(self.colorText))
+                textRect1 = labelText1.get_rect(topleft=(125,40))
+                textRect2 = labelText2.get_rect(topleft=(125,60))
+                textRect3 = labelText3.get_rect(topleft=(125,80))
+                self.root.blit(labelText1,textRect1)
+                self.root.blit(labelText2,textRect2)
+                self.root.blit(labelText3,textRect3)        
+                self._affichageEntre(True)
+            else : 
+                guiParole = self.paroleGUI[1] #Small
+                self.root.blit(guiParole.convert(),(0,0))
+                labelText1 =  self.police.render(text1,1,(self.colorText))
+                labelText2 =  self.police.render(text2,1,(self.colorText))
+                textRect1 = labelText1.get_rect(topleft=(130,90))
+                textRect2 = labelText2.get_rect(topleft=(130,110))
+                self.root.blit(labelText1,textRect1)
+                self.root.blit(labelText2,textRect2)
+                self._affichageEntre(False)
+        else :
+            guiParole = self.paroleGUI[1] #Small
+            self.root.blit(guiParole.convert(),(0,0))
+            labelText =  self.police.render(texte,1,(self.colorText))
+            textRect = labelText.get_rect(topleft=(130,90))
+            self.root.blit(labelText,textRect)
+            self._affichageEntre(False)
+        pygame.display.update()
+      
+    def _affichageEntre(self,big:bool):
+        texte = self.oldRequette
+        if self._compteur(texte) > 6 :
+            texte1,texte2 = self._division(texte)
+            labelRequette1 = self.police.render(texte1,1,(self.colorText))
+            labelRequette2 = self.police.render(texte2,1,(self.colorText))
+            if big == True :
+               requetteRect1 = labelRequette1.get_rect(topleft=(130,360)) 
+               requetteRect2 = labelRequette2.get_rect(topleft=(130,380))
+               
+            else :
+                requetteRect1 = labelRequette1.get_rect(topleft=(130,350)) 
+                requetteRect2 = labelRequette2.get_rect(topleft=(130,370))
+            self.root.blit(labelRequette1,requetteRect1)
+            self.root.blit(labelRequette2,requetteRect2)   
+        else :
+            labelRequette = self.police.render(texte,1,(self.colorText))
+            if big == True :
+               requetteRect = labelRequette.get_rect(topleft=(130,370))
+            else :
+                requetteRect = labelRequette.get_rect(topleft=(130,350))
+            self.root.blit(labelRequette,requetteRect)
+      
+    def saveValMicro(self,requette:str):
+        self.oldRequette = requette
+        
+    def _division(self,text, nombre):
+        mots = text.split()
+        premierPartie = mots[:nombre]
+        deuxiemePartie = mots[nombre:]
+        return ' '.join(premierPartie), ' '.join(deuxiemePartie)
+    
+    def _compteur(self,s:str):
+        mots = s.split()
+        return int(len(mots))
