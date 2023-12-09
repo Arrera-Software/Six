5497ca8064f5aceeb4444eac7883e366a975ec8d
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-12-10 00:06:38 +0100

    ajout du methode pour gerer la nouvelle version du reseau de neuron (NON Tester)(Pas Fini)

--- src/SixTK.py
+++ src/SixTK.py
@@ -231,3 +231,40 @@ class SixTKMain :
     
     def destroyWindows(self):
         self.windows.destroy()
+
+    def __formatageText(self,texte):
+        if int(len(texte)) > 6 :
+            texte1,texte2 = self.__division(texte,6)
+            allTexte = texte1+"\n"+texte2
+            if int(len(texte2)) > 6 :
+                texte2,texte3 = self.__division(texte2,6)
+                allTexte = texte1+"\n"+texte2+"\n"+texte3
+                if int(len(texte3)) > 6 :
+                    texte3,texte4 = self.__division(texte3,6)
+                    allTexte = texte1+"\n"+texte2+"\n"+texte3+"\n"+texte4
+        else :
+            allTexte = texte
+        return str(allTexte)
+    
+    def vueActu(self,sortie:list,valeur:int):
+        color = self.__gestionnaire.getColorInterface()
+        colorText = self.__gestionnaire.getGUItextColor()
+        windows = Tk()
+        windows.title("")
+        windows.maxsize(500,600)
+        windows.minsize(500,600)
+        windows.configure(bg=color)
+        labelActu = Label(windows,bg=color,fg=colorText)
+        labelActu.place(x="80",y="5")
+        if (valeur==3):
+            text = self.__formatageText(sortie[0])+"\n\n"+self.__formatageText(sortie[1])+"\n\n"+self.__formatageText(sortie[2])
+            labelActu.configure(text=text, anchor="w")
+        else :
+            if valeur == 11 :
+                print("error")
+            else :
+                if valeur == 12 :
+                    text = self.__formatageText(sortie[0])+"\n"+self.__formatageText(sortie[1])+"\n La fete du jour est : "+self.__formatageText(sortie[2])+"\n"+self.__formatageText(sortie[3])+"\n"+self.__formatageText(sortie[4])+"\n\n"+self.__formatageText(sortie[5])
+                    labelActu.configure(text=text, anchor="w")
+
+        
