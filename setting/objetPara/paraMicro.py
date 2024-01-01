9d1419e8d134c30bbf0813823e19be03ee3c6fe9
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-01-01 18:26:37 +0100

    passage attribut en private

--- setting/objetPara/paraMicro.py
+++ setting/objetPara/paraMicro.py
@@ -5,37 +5,37 @@ from tkinter import messagebox
 class SettingMicro :
     def __init__(self,cadre:Frame,SixConfig:jsonWork,textColor:str,color:str):
         #variable
-        self.config = SixConfig
+        self.__config = SixConfig
         #declaration cadre
-        self.mainFrame = cadre
-        self.acceuilFrame = Frame(self.mainFrame,bg=color,width=350,height=600)
+        self.__mainFrame = cadre
+        self.__acceuilFrame = Frame(self.__mainFrame,bg=color,width=350,height=600)
         #widget 
-        labelTitre = Label(self.acceuilFrame,text="Sons au declanchement du micro",bg=color,fg=textColor,font=("arial","20"))
-        self.btnOnOff = Button(self.acceuilFrame,bg=color,fg=textColor,font=("arial","20"))
+        labelTitre = Label(self.__acceuilFrame,text="Sons au declanchement du micro",bg=color,fg=textColor,font=("arial","20"))
+        self.__btnOnOff = Button(self.__acceuilFrame,bg=color,fg=textColor,font=("arial","20"))
         #calcule Affichage
-        largeur = self.acceuilFrame.winfo_reqwidth()
-        hauteur = self.acceuilFrame.winfo_reqheight()
+        largeur = self.__acceuilFrame.winfo_reqwidth()
+        hauteur = self.__acceuilFrame.winfo_reqheight()
         self.setTextButton()
         #affichage
         labelTitre.place(x=(largeur-labelTitre.winfo_reqwidth()),y=0)
-        self.btnOnOff.place(relx=0.5,rely=0.5,anchor="center")
+        self.__btnOnOff.place(relx=0.5,rely=0.5,anchor="center")
 
     def view(self):
-        self.mainFrame.pack(side="left")
-        self.acceuilFrame.place(x=0,y=0)
+        self.__mainFrame.pack(side="left")
+        self.__acceuilFrame.place(x=0,y=0)
 
     def setTextButton(self):
-        valeur = int(self.config.lectureJSON("soundMicro"))
+        valeur = int(self.__config.lectureJSON("soundMicro"))
         if valeur == 1 :
-            self.btnOnOff.configure(text="OFF",command=self.commandOff)
+            self.__btnOnOff.configure(text="OFF",command=self.commandOff)
         else :
             if valeur == 0 :
-                self.btnOnOff.configure(text="ON",command=self.commandOn)
+                self.__btnOnOff.configure(text="ON",command=self.commandOn)
 
     def commandOff(self):
-        self.config.EcritureJSON("soundMicro","0")
+        self.__config.EcritureJSON("soundMicro","0")
         self.setTextButton()
     
     def commandOn(self):
-        self.config.EcritureJSON("soundMicro","1")
+        self.__config.EcritureJSON("soundMicro","1")
         self.setTextButton()
