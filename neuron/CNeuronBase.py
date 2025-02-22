dd5cdd83c2734e3f04481b6209f81f514b0d680c
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-02-22 11:35:06 +0100

    Ajout CNeuronBase.py

---
+++ neuron/CNeuronBase.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+from ObjetsNetwork.gestion import*
+from arreraSoftware.fncArreraNetwork import*
+from ObjetsNetwork.chaineCarractere import*
+from ObjetsNetwork.enabledNeuron import*
+from ObjetsNetwork.historique import*
+
+class neuronBase :
+    def __init__(self, fncArreraNetwork:fncArreraNetwork, gestionnaire:gestionNetwork,objHist:CHistorique):
+        #Init objet
+        self._gestionNeuron = gestionnaire
+        self._fonctionArreraNetwork = fncArreraNetwork
+        self._gestNeuron = self._gestionNeuron.getEtatNeuronObjet()
+        self._objHistorique = objHist
+        self._listSortie = ["",""]
+        self._valeurOut = 0
+
+    def getListSortie(self)->list:
+        return self._listSortie
+
+    def getValeurSortie(self)->int :
+        return self._valeurOut
+
+    @abstractmethod
+    def neurone(self,requette:str):
+        pass
