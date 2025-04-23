b81ad91b2b6ebc9948146c4c0f5ebbb05604d00e
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-04-23 21:01:57 +0200

    Mise en place du boot pour app image

--- src/sixBoot.py
+++ src/sixBoot.py
@@ -1,8 +1,8 @@
 from lynx.arreraLynx import*
-from librairy.dectectionOS import*
 from src.SixGUI import*
 import os
 import shutil
+from src.CTigerDemon import *
 
 class SixBoot :
     def __init__(self):
@@ -13,6 +13,7 @@ class SixBoot :
         self.__sortieLynx = False
         self.__firstStart = False
         self.__os = OS()
+        self.__demonTiger = CTigerDemon("six","https://arrera-software.fr/depots.json")
 
         if (self.__os.osWindows() == True):
             # Verification de la configuration de l'assistant
@@ -24,10 +25,25 @@ class SixBoot :
             del json
         elif (self.__os.osLinux() == True):
             self.__destUser = os.path.expanduser("~/.config/six/configUser.json")
-            if not os.path.exists(self.__destUser):
+            self.__destEnvent = os.path.expanduser("~/.config/six/evenementUser.json")
+            self.__destTache = os.path.expanduser("~/.config/six/tache.json")
+            self.__destHist = os.path.expanduser("~/.config/six/neuronHist.json")
+            if (not os.path.exists(self.__destUser) or not os.path.exists(self.__destEnvent)
+                    or not os.path.exists(self.__destHist) or not os.path.exists(self.__destTache)):
                 os.makedirs(os.path.dirname(self.__destUser), exist_ok=True)
                 shutil.copyfile("FileJSON/configUser.json",
                                 self.__destUser)
+                shutil.copyfile("FileJSON/evenementUser.json",
+                                self.__destEnvent)
+                shutil.copyfile("FileJSON/neuronHist.json",
+                                self.__destHist)
+                shutil.copyfile("FileJSON/tache.json",
+                                self.__destTache)
+
+                user = jsonWork(self.__destUser)
+                user.EcritureJSON("emplacementEvenenement",self.__destEnvent)
+                user.EcritureJSON("emplacementTache",self.__destTache)
+                del user
                 self.__firstStart = True
             else :
                 self.__firstStart = False
@@ -47,8 +63,9 @@ class SixBoot :
 
 
     def __boot(self):
+        arrTk = CArreraTK()
+        self.__checkUpdate(arrTk)
         if (self.__sortieLynx == False):
-            arrTk = CArreraTK()
             screen = arrTk.aTK(title="Arrera Six",resizable=False,width=500,height=350)
             imgCavas = arrTk.createArreraBackgroudImage(screen,
                                                         imageDark="asset/IMGinterface/dark/NoConfig.png",
@@ -72,19 +89,47 @@ class SixBoot :
                                "FileJSON/sixConfig.json",
                                "FileJSON/configUser.json",
                                "FileJSON/configNeuron.json",
-                               "FileJSON/configSetting.json")
+                               "FileJSON/configSetting.json"
+                               ,self.__demonTiger.getVersionSoft())
 
             assistant.active(self.__firstStart)
         elif (self.__os.osLinux() == True):
             assistant = SixGUI("asset/icon/",
                                "icon",
                                "FileJSON/sixConfig.json",
-                               "~/.config/six/configUser.json",
+                               self.__destUser,
                                "FileJSON/configNeuron.json",
-                               "FileJSON/configSetting.json")
+                               "FileJSON/configSetting.json"
+                               ,self.__demonTiger.getVersionSoft())
 
             assistant.active(self.__firstStart)
 
     def __restartConf(self,windows:ctk.CTk):
         windows.destroy()
         self.active()
+
+    def __checkUpdate(self,arrTk:CArreraTK):
+
+        if (self.__demonTiger.checkUpdate()):
+            screen = arrTk.aTK(title="Arrera Six",resizable=False,width=500,height=350)
+            imgCavas = arrTk.createArreraBackgroudImage(screen,imageDark="asset/IMGinterface/dark/MAJ.png",
+                                                        imageLight="asset/IMGinterface/white/MAJ.png",
+                                                        width=500,height=350)
+            labeltext = arrTk.createLabel(screen,text="Une mise à jour d'ARRERA SIX est disponible. Installez-la pour bénéficier des dernières fonctionnalités.",
+                                          ppolice="Arial",ptaille=20
+                                          ,pstyle="bold",bg="#2b3ceb",
+                                          fg="white",pwraplength=250,
+                                          justify="left")
+            btnUpdate = arrTk.createButton(screen,text="Mettre a jour",
+                                           ppolice="Arial",ptaille=20,
+                                           pstyle="bold",
+                                           command=lambda :wb.open("https://www.github.com/Arrera-Software/Six/releases"))
+            btnContinuer = arrTk.createButton(screen,text="Me rappeler plus tart",
+                                              ppolice="Arial",ptaille=20,
+                                              pstyle="bold",
+                                              command=lambda : screen.destroy())
+            imgCavas.pack()
+            labeltext.place(x=190,y=40)
+            arrTk.placeBottomLeft(btnUpdate)
+            arrTk.placeBottomRight(btnContinuer)
+            arrTk.view()
