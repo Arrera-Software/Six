c60b9930d2646a47d2a93a1d401c23ef593975a0
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-17 19:37:07 +0100

    Fixe fermeture parametre

--- src/languageSIX.py
+++ src/languageSIX.py
@@ -7,70 +7,70 @@ class CLanguageSIX :
         self.__firstBoot = jsonWork(fileFirstBoot)
 
     def getPhQuitSetting(self):
-        return self.__language.lectureJSON("phQuitSetting")
+        return self.__language.getContentJsonFlag("phQuitSetting")
 
     def getPhQuitActu(self):
-        return self.__language.lectureJSON("phQuitActu")
+        return self.__language.getContentJsonFlag("phQuitActu")
 
     def getPhActiveMute(self):
-        return self.__language.lectureJSON("phActiveMute")
+        return self.__language.getContentJsonFlag("phActiveMute")
 
     def getPhQuitMute(self):
-        return self.__language.lectureJSON("phQuitMute")
+        return self.__language.getContentJsonFlag("phQuitMute")
 
     def getphErreurResumer(self):
-        return self.__language.lectureJSON("phErreurResumer")
+        return self.__language.getContentJsonFlag("phErreurResumer")
 
     def getPhOpenResumerActu(self):
-        return self.__language.lectureJSON("phOpenResumerActu")
+        return self.__language.getContentJsonFlag("phOpenResumerActu")
 
     def getPhOpenActu(self):
-        return self.__language.lectureJSON("phOpenActu")
+        return self.__language.getContentJsonFlag("phOpenActu")
 
     def getPhErreurActu(self):
-        return self.__language.lectureJSON("phErreurActu")
+        return self.__language.getContentJsonFlag("phErreurActu")
 
     def getPhReadDocument(self):
-        return self.__language.lectureJSON("phReadDocument")
+        return self.__language.getContentJsonFlag("phReadDocument")
 
     def getPhReadTableur(self):
-        return self.__language.lectureJSON("phReadTableur")
+        return self.__language.getContentJsonFlag("phReadTableur")
 
     def getPhOpenResumerAgendaTache(self):
-        return self.__language.lectureJSON("phOpenResumerAgendaTache")
+        return self.__language.getContentJsonFlag("phOpenResumerAgendaTache")
 
     def getPhOpenResumerAll(self):
-        return self.__language.lectureJSON("phOpenResumerAll")
+        return self.__language.getContentJsonFlag("phOpenResumerAll")
 
     def getPhErreurResumerAll(self):
-        return self.__language.lectureJSON("phErreurResumerAll")
+        return self.__language.getContentJsonFlag("phErreurResumerAll")
 
     def getPhOpenAideTableur(self):
-        return self.__language.lectureJSON("phOpenAideTableur")
+        return self.__language.getContentJsonFlag("phOpenAideTableur")
 
     def getPhOpenAideWord(self):
-        return self.__language.lectureJSON("phOpenAideWord")
+        return self.__language.getContentJsonFlag("phOpenAideWord")
 
     def getPhOpenAideFichier(self):
-        return self.__language.lectureJSON("phOpenAideFichier")
+        return self.__language.getContentJsonFlag("phOpenAideFichier")
 
     def getPhOpenAideRadio(self):
-        return self.__language.lectureJSON("phOpenAideRadio")
+        return self.__language.getContentJsonFlag("phOpenAideRadio")
 
     def getPhOpenAideProjet(self):
-        return self.__language.lectureJSON("phOpenAideProjet")
+        return self.__language.getContentJsonFlag("phOpenAideProjet")
 
     def getPhOpenAideWork(self):
-        return self.__language.lectureJSON("phOpenAideWork")
+        return self.__language.getContentJsonFlag("phOpenAideWork")
 
     def getHelpTableur(self):
-        return self.__help.lectureJSONList("tableur")
+        return self.__help.getContentJsonFlagList("tableur")
 
     def getHelpWord(self):
-        return self.__help.lectureJSONList("word")
+        return self.__help.getContentJsonFlagList("word")
 
     def getHelpProjet(self):
-        return self.__help.lectureJSONList("projet")
+        return self.__help.getContentJsonFlagList("projet")
 
-    def getPhraseFirstBoot(self,genre:str,user:str,nb:int):
-        return self.__firstBoot.lectureJSON(str(nb)).format(genre=genre,user=user)
+    def getPhraseFirstBoot(self, genre:str, name:str, nb:int):
+        return self.__firstBoot.getContentJsonFlag(str(nb)).format(genre=genre, user=name)
